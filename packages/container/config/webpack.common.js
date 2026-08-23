a026ea202b6a04340f3099bb829c2d265da54e6f
milindrev <milindrev@gmail.com>
2026-08-23 13:48:17 -0400

    Created Container deploy scrpt

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,17 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.(js|jsx|mjs)$/, // 1. FIXED: Added jsx to the regex
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
+            // 2. FIXED: Added runtime automatic to prevent React UMD errors
+            presets: [
+              ["@babel/preset-react", { runtime: "automatic" }],
+              "@babel/preset-env",
+            ],
             plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
     ],
   },
+  resolve: {
+    extensions: [".js", ".jsx", ".mjs"], // 3. FIXED: Allows importing files without writing the extension
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
