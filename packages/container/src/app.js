1553b750ddbd0cb554e7cf9312b8a98f273cf76f
milindrev <milindrev@gmail.com>
2026-08-23 14:32:49 -0400

    Updated App.js to trigger build action

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -4,7 +4,7 @@ import MarketingApp from "./component/MarketingApp";
 export default function App() {
   return (
     <div>
-      <h1>Hi There ... container App</h1>
+      <h1>Hi There ... This is container App js</h1>
       <hr />
       <MarketingApp />
     </div>
