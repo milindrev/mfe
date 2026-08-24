49c6683b89c5dd98648a61dbecf53b56e0a94e50
milindrev <milindrev@gmail.com>
2026-08-24 15:54:43 -0400

    added public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPatha: "/container/latest/",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
