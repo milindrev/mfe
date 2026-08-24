a87e14f257e157db84ad7409f2f626104f724cb6
milindrev <milindrev@gmail.com>
2026-08-24 17:33:31 -0400

    fixed typo in build command in package.json

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -4,7 +4,7 @@ import MarketingApp from "./component/MarketingApp";
 export default function App() {
   return (
     <div>
-      <h1>Hi there!! This is the Index document for container App js</h1>
+      <h1>Hello there!! This is the Index document for container App js</h1>
       <hr />
       <MarketingApp />
     </div>
